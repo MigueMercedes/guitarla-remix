12585de849eb68ffab49db50bb9d9ed7e4684352
Miguel Mercedes <lmyguel0324@gmail.com>
2023-10-11 15:15:34 -0400

    Guitars dinamic routes

---
+++ app/routes/guitarras.$guitarraId.jsx
@@ -0,0 +1,60 @@
+import { useLoaderData } from '@remix-run/react';
+import { getGuitarraById } from '@/api/guitarras.server'
+import styles from '@/styles/guitarras.css'
+
+export function meta({data}) {
+
+  console.log(data)
+
+  const title = `GuitarLA - ${data.attributes.nombre}`;
+  const description = `Guitarras, venta de guitarras, guitarra ${data.attributes.nombre}`
+
+  return [
+    {
+      title,
+      description
+    }
+  ]
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async function loader({params}) {
+  
+  const { guitarraId } = params;
+  const guitarra = await getGuitarraById( guitarraId );
+  return guitarra.data[0];
+}
+
+function Guitarra() {
+
+  const guitarra = useLoaderData();
+  const { nombre, descripcion, imagen, precio } = guitarra.attributes;
+
+  return (
+    <main className="contenedor guitarra">
+
+      <img 
+        className='imagen' 
+        src={imagen.data.attributes.url} 
+        alt={`Imagen de ${nombre}`} 
+      />
+
+      <div className="contenido">
+        <h3>{ nombre }</h3>
+        <p className="texto">{ descripcion }</p>
+        <p className="precio">${ precio }</p>
+      </div>
+      
+    </main>
+  )
+}
+
+export default Guitarra
--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,13 +1,50 @@
+import { useLoaderData } from "@remix-run/react";
+import Guitarra from "@/components/guitarra";
+import { getGuitarras } from "@/api/guitarras.server";
+import styles from '@/styles/guitarras.css';
+
+export function meta() {
+  return [
+    {
+      title: 'GuitarLA - Tienda de Guitarras',
+      description: 'GuitarLA - Nuestra colección de guitarras'
+    }
+  ]
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 export async function loader() {
-  const respuesta = await fetch('http://127.0.0.1:1337/api/guitarras?populate=imagen');
-  const resultado = await respuesta.json();
-  console.log(resultado);
-  return {}
+  const guitarras = await getGuitarras();
+  return guitarras.data;
 }
 
 function Tienda() {
+
+  const guitarras = useLoaderData();
+
   return (
-    <div>Tienda</div>
+    <main className="contenedor">
+      <h2 className="heading">Nuestra Colección</h2>
+      
+      {guitarras?.length > 0 ? (
+        <div className="guitarras-grid">
+          {guitarras.map( guitarra => (
+            <Guitarra
+              key={guitarra?.attributes.url}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      ): 'No hay guitarras actualmente' }
+
+    </main>
   )
 }
 
